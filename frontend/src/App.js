6cc3bd708d7fae9265a3485cee79305be2b0a867
vojtahaj <vojta.haj@gmail.com>
2019-05-14 13:30:42 +0200

    DrawerReactJS
    Vytvoreni responzivniho draweru pro menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,19 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleTable from './components/SimpleTable'
+import ResponsiveDrawer from './components/ResponsiveDrawer'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
 
-                <SimpleTable />
+                {/*<SimpleTable />*/}
+                <ResponsiveDrawer/>
             </div>
         );
     }
 }
 
 export default App;
-
-class ShoppingList extends Component {
-    render() {
-        return (<div className="shoppingList">
-                <h1>shoppinglist pro {this.props.name}</h1>
-                <ul>
-                    <li>polozka 1</li>
-                    <li>polozka 2</li>
-                    <li>polozka 3</li>
-                </ul>
-            </div>
-        )
-    }
-}
 
---
+++ frontend/src/components/ResponsiveDrawer.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SimpleTable from './SimpleTable'
+import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios'
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    menuButton: {
+        marginRight: 20,
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
+
+class ResponsiveDrawer extends React.Component {
+    state = {
+        mobileOpen: false,
+    };
+
+    handleDrawerToggle = () => {
+        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+    };
+
+    // Načtení veškerých týmů v DB:
+    loadZavody = async () => {
+        await axios.get("/zavody/zavody_all")
+            .then(async response => {
+                await this.setState({teams: response.data});
+            })
+            .catch(async error => {
+                console.log("An error occurred while trying to load races from DB: " + error);
+
+
+                await console.log(error.response.data);
+            })
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+
+        const drawer = (
+            <div>
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    {['Závody', 'Live', 'Kalendář', 'O nás'].map((text, index) => (
+                        <ListItem button key={text}>
+
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        );
+
+        return (
+            <div className={classes.root}>
+                //<CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                            className={classes.menuButton}
+                        >
+                            {/*<MenuIcon />*/}
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Responsive drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer}>
+                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                    <Hidden smUp implementation="css">
+                        <Drawer
+                            container={this.props.container}
+                            variant="temporary"
+                            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                            open={this.state.mobileOpen}
+                            onClose={this.handleDrawerToggle}
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                </nav>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <SimpleTable />
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+                        elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                        hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+                        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+                        Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+                        viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+                        Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+                        at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+                        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+                    </Typography>
+                </main>
+            </div>
+        );
+    }
+}
+
+ResponsiveDrawer.propTypes = {
+    classes: PropTypes.object.isRequired,
+    // Injected by the documentation to work in an iframe.
+    // You won't need it on your project.
+    container: PropTypes.object,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
